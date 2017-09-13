0b92c5f1c3c387b3d66857c82c42034c9b22289c
Alan Costa <AC004006@systex.the-espgroup.com>
2017-09-13 16:18:21 +0100

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test
+{
+    internal partial class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Utilities.GetUserProfile()); do
+            {
+                while (!Console.KeyAvailable)
+                {
+
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
+;
---
+++ test/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace test
+{
+    internal partial class Program
+    {
+        public class Utilities
+        {
+            public Utilities()
+            {
+            }
+
+            public static string GetUserProfile()
+            {
+                return Path.GetFileName(Environment.GetEnvironmentVariable("USERPROFILE"));
+            }
+        }
+    }
+}
+;
