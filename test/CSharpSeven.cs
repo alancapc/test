e50b157ec5086dec97808a907736a115e04edc8b
Alan Costa <carlosalan.pc@gmail.com>
2017-09-15 22:58:08 +0100

    fail build

--- test/CSharpSeven.cs
+++ test/CSharpSeven.cs
@@ -57,7 +57,7 @@ namespace test
             }
         }
 
-        public static void TupleValueTupleReturn()
+        public static vod TupleValueTupleReturn()
         {
             var result = ReturnTuple();
 
