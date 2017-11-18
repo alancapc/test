2ab581c671474dbac0e58fcd975886cb55135fbc
Alan Costa <carlosalan.pc@gmail.com>
2017-11-18 23:09:40 +0000

    removing wrong api project

--- Aspnetcorewebapi/Startup.cs
+++ Aspnetcorewebapi/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Aspnetcorewebapi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetCoreWebApi
 {
@@ -27,6 +25,7 @@ namespace aspnetCoreWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(option => option.u)
             // Add framework services.
             services.AddMvc();
         }
