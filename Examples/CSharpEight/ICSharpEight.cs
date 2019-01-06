76d0416f04315d1ecae81b417fa19b9f5fec4aaf
Alan Costa <carlosalan.pc@gmail.com>
2019-01-06 00:43:34 +0000

    added interface of examples to be implemented

---
+++ Examples/CSharpEight/ICSharpEight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.CSharpEight
+{
+    interface ICSharpEight
+    {
+        void NullableReferenceTypes();
+
+        void AsyncStreams();
+
+        void RagesAndIndices();
+
+        void DefaultImplementationsOfInterfaceMembers();
+
+        void RecursivePatterns();
+
+        void SwitchExpressions();
+
+        void TargetTypedNewExpressions();
+    }
+}
